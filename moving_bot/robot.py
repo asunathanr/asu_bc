b48314d2afe829fb42d904b07ac5f576ddccda08
Nathan Robertson <absnathanr@gmail.com>
2019-01-08 16:08:24 -0600

    Added UnitTypes enum

--- moving_bot/robot.py
+++ moving_bot/robot.py
@@ -1,4 +1,5 @@
 from battlecode import BCAbstractRobot, SPECS
+from enum import Enum
 import battlecode as bc
 import random
 
@@ -9,21 +10,43 @@ __pragma__('tconv')
 
 """
 Requirements:
-    1. Should be able to pick a destination in its vision radius.
-    2. Should be able to move to destination
-    3. If destination contains resource should pick up resource
-    4. Store accumulated resources in nearest castle
+    1. Should know its neighbors.
+    2. Should pick a destination from its neighbors.
+    3. Should be able to move to destination
+    4. If destination contains resource should pick up resource
+    5. Store accumulated resources in nearest castle
 As always requirements are fluid and subject to change at any time.
 """
 
 
+class UnitTypes(Enum):
+    """
+    From documentation:
+    r.unit: The robot's unit type, where
+     0 stands for Castle,
+     1 stands for Church,
+     2 stands for Pilgrim,
+     3 stands for Crusader,
+     4 stands for Prophet,
+      and 5 stands for Preacher. Available if visible.
+    """
+    CASTLE = 0
+    CHURCH = 1
+    KNIGHT = 2
+    CRUSADER = 3
+    PROPHET = 4
+    PREACHER = 5
+
+
 # don't try to use global variables!!
-class MyRobot(BCAbstractRobot):
+class Unit(BCAbstractRobot):
     step = -1
 
     def turn(self):
         self.step += 1
-        self.log("START TURN " + self.step)
-        self.log("Castle health: " + self.me['health'])
 
-robot = MyRobot()
+    def neighbors(self):
+        pass
+
+
+robot = Unit()
