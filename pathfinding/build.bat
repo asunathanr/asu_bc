854d963efd2a345ab3f4392abae9d665f5c1d8db
Nathan Robertson <absnathanr@gmail.com>
2019-01-14 20:39:52 -0600

    Moved binary heap to its own file

--- pathfinding/build.bat
+++ pathfinding/build.bat
@@ -1,4 +1,5 @@
 copy "robot.js" "c:/Users/absna/AppData/Roaming/npm/node_modules/bc19/bots/my_bot"
+copy "binary_heap.js" "c:/Users/absna/AppData/Roaming/npm/node_modules/bc19/bots/my_bot"
 copy "nav.js" "c:/Users/absna/AppData/Roaming/npm/node_modules/bc19/bots/my_bot"
 cd "c:/Users/absna/AppData/Roaming/npm/node_modules/bc19/bots"
 bc19run -r my_bot -b my_bot
