fe2ab42efa7bb233f6d059bfd90c8a9023de6d30
Nathan Robertson <absnathanr@gmail.com>
2019-01-19 14:55:55 -0600

    Added other unit speeds

--- state_bot/constants.js
+++ state_bot/constants.js
@@ -3,14 +3,19 @@ import { SPECS } from './battlecode';
 /**
  * @file speeds.js
  * @author Nathan Robertson
- * Defines speed constants
+ * Defines project constants
  */
 
  const CONSTANTS = {};
 
  CONSTANTS.CRUSADER_SPEED = SPECS.UNITS[SPECS.CRUSADER].SPEED;
  CONSTANTS.PILGRIM_SPEED = SPECS.UNITS[SPECS.PILGRIM].SPEED;
+ CONSTANTS.PROPHET_SPEED = SPECS.UNITS[SPECS.PROPHET].SPEED;
+ CONSTANTS.PREACHER_SPEED = SPECS.UNITS[SPECS.PREACHER].SPEED;
+
  
  CONSTANTS.ADJACENT_DELTAS = [[0, 1], [0, -1], [1, 0], [-1, 0], [-1, 1], [1, -1], [-1, -1], [1, 1]];
+
+ CONSTANTS.DEBUG = true;
  
  export default CONSTANTS;
